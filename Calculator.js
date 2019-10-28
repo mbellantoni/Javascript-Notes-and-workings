306725adbbe31be46e176250aef9d65a4edf54a6
mbellantoni <53529228+mbellantoni@users.noreply.github.com>
2019-10-28 16:09:23 -0400

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -12,3 +12,8 @@ function counter (arr, add) {
  return arr.map(a => a + add);
 	}
 console.log(counter([5,0,4], 10));
+
+function counter(arr, criteria) {
+		return arr.filter( a => a > criteria);
+}
+console.log(counter([5,0,4], 1));
